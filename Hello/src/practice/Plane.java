c06ff0923a5b92f8b213c0aa0690bee60d698fde
hayelee <ezezezi94@gmail.com>
2022-08-24 10:38:09 +0900

    plane

---
+++ Hello/src/practice/Plane.java
@@ -0,0 +1,53 @@
+package practice;
+
+public class Plane {
+	// 필드
+	private String manufacture;
+	private String model;
+	private int maxNumberOfPassengers;
+	private static int numberOfPlanes;
+
+	// 생성자
+	public Plane() {
+		numberOfPlanes++;
+	}
+
+	public Plane(String manufacture, String model, int maxNumberOfPassengers) {
+		this();
+		this.manufacture = manufacture;
+		this.model = model;
+		this.maxNumberOfPassengers = maxNumberOfPassengers;
+		numberOfPlanes++;
+	}
+
+	// 메소드
+	
+	public String getManufacture() {
+		return manufacture;
+	}
+
+	public void setManufacture(String manufacture) {
+		this.manufacture = manufacture;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+
+	public int getMaxNumberOfPassengers() {
+		return maxNumberOfPassengers;
+	}
+
+	public void setmaxNumberOfPassengers(int maxNumberOfPassengers) {
+		if (maxNumberOfPassengers > 0)
+			this.maxNumberOfPassengers = maxNumberOfPassengers;
+	}
+
+	public static int getnumberOfPlanes() {
+		return numberOfPlanes;
+	}
+}
